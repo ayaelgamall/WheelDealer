7e6a3847186660fa0da5af366e24752ca1cdb33b
MoamenShoman <moamenkshoman@gmail.com>
2022-12-26 17:52:02 +0200

    Finalized messages screen UI

--- lib/widgets/settings_widget.dart
+++ lib/widgets/settings_widget.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:bar2_banzeen/screeens/settings_screen.dart';
 import 'package:flutter/material.dart';
-import '../screeens/settings_screen.dart';
 
 class SettingsWidget extends StatelessWidget {
   const SettingsWidget({super.key});
@@ -11,7 +9,7 @@ class SettingsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return IconButton(
       onPressed: (() {
-        Navigator.of(context).pushNamed(SettingsScreen.routeName);
+        null;
       }),
       icon: Icon(Icons.settings_outlined),
       color: Color(0xFFCCCCCC),
