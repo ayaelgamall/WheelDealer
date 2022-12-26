4106cc7ac80f3fab6808dec9377eee4fa3a621ec
Mohammad Hossam <60348524+mohammadhossam@users.noreply.github.com>
2022-12-26 11:27:37 +0200

    Supported realtime chat updates

--- lib/screeens/messaging.dart
+++ lib/screeens/messaging.dart
@@ -30,25 +30,25 @@ class _MessagingScreenState extends State<MessagingScreen> {
   void initializeFirebase() async {
     try {
       await Firebase.initializeApp();
-      QuerySnapshot<Map<String, dynamic>> chatDocs = await FirebaseFirestore
-          .instance
-          .collection("chats")
-          .where('users', arrayContainsAny: [thisUserId]).get();
+      // QuerySnapshot<Map<String, dynamic>> chatDocs = await FirebaseFirestore
+      //     .instance
+      //     .collection("chats")
+      //     .where('users', arrayContainsAny: [thisUserId]).get();
 
-      List<Stream<List<ChatTile>>> streamList = [];
-      for (QueryDocumentSnapshot<Map<String, dynamic>> chatDoc
-          in chatDocs.docs) {
-        chatTilesStream = chatDocs.docs.first.reference
-            .collection('messages')
-            .orderBy("time", descending: true)
-            .limit(1)
-            .snapshots()
-            .asyncMap((msgDocs) => Future.wait([
-                  for (var msgDoc in msgDocs.docs) generateUsersInChats(msgDoc)
-                ]));
-        streamList.add(chatTilesStream);
-      }
-      finalChatTilesStream = StreamGroup.merge(streamList);
+      // List<Stream<List<ChatTile>>> streamList = [];
+      // for (QueryDocumentSnapshot<Map<String, dynamic>> chatDoc
+      //     in chatDocs.docs) {
+      //   chatTilesStream = chatDocs.docs.first.reference
+      //       .collection('messages')
+      //       .orderBy("time", descending: true)
+      //       .limit(1)
+      //       .snapshots()
+      //       .asyncMap((msgDocs) => Future.wait([
+      //             for (var msgDoc in msgDocs.docs) generateUsersInChats(msgDoc)
+      //           ]));
+      //   streamList.add(chatTilesStream);
+      // }
+      // finalChatTilesStream = StreamGroup.merge(streamList);
 
       setState(() {
         isIntialized = true;
@@ -60,38 +60,61 @@ class _MessagingScreenState extends State<MessagingScreen> {
     }
   }
 
-  Future<ChatTile> generateUsersInChats(
-      QueryDocumentSnapshot<Map<String, dynamic>> msgsDoc) async {
-    Map<String, dynamic>? lastMsgData = msgsDoc.data();
-
-    String userId = lastMsgData['to'] != thisUserId
-        ? lastMsgData['to']
-        : lastMsgData['from'];
-    DocumentSnapshot<Map<String, dynamic>> userDoc =
-        await FirebaseFirestore.instance.collection("users").doc(userId).get();
-    Map<String, dynamic>? userData = userDoc.data();
-
-    // QuerySnapshot<Map<String, dynamic>> msgsCol = await chatDoc.reference
-    //     .collection("messages")
-    //     .orderBy("time", descending: true)
-    //     .get();
-    // QueryDocumentSnapshot<Map<String, dynamic>> lastMsg = msgsCol.docs.first;
-    // Map<String, dynamic>? lastMsgData = lastMsg.data();
-    // print(lastMsgData);
-    // //getting the user data
-    // List usersIds = chatDoc.data()['users'];
-    // String userId = usersIds[0] != thisUserId ? usersIds[0] : usersIds[1];
-    // DocumentSnapshot<Map<String, dynamic>> userDoc =
-    //     await FirebaseFirestore.instance.collection("users").doc(userId).get();
-    // Map<String, dynamic>? userData = userDoc.data();
-    // print(userData);
-
-    return ChatTile(
-        username: userData?['username'],
-        profilePhotoLink: userData?['profile_photo'],
-        lastMsgText: lastMsgData['text'],
-        time: lastMsgData['time']);
+  Stream<QuerySnapshot> getChats() {
+    return FirebaseFirestore.instance
+        .collection("chats")
+        .where('users', arrayContainsAny: [thisUserId]).snapshots();
   }
+
+  Stream<QuerySnapshot> getLastMessages(String chatId) {
+    return FirebaseFirestore.instance
+        .collection("chats")
+        .doc(chatId)
+        .collection("messages")
+        .orderBy("time", descending: true)
+        .limit(1)
+        .snapshots();
+  }
+
+  Stream<DocumentSnapshot<Map<String, dynamic>>> getUser(String userId) {
+    return FirebaseFirestore.instance
+        .collection("users")
+        .doc(userId)
+        .snapshots();
+  }
+
+  // Future<ChatTile> generateUsersInChats(
+  //     QueryDocumentSnapshot<Map<String, dynamic>> msgsDoc) async {
+  //   Map<String, dynamic>? lastMsgData = msgsDoc.data();
+
+  //   String userId = lastMsgData['to'] != thisUserId
+  //       ? lastMsgData['to']
+  //       : lastMsgData['from'];
+  //   DocumentSnapshot<Map<String, dynamic>> userDoc =
+  //       await FirebaseFirestore.instance.collection("users").doc(userId).get();
+  //   Map<String, dynamic>? userData = userDoc.data();
+
+  //   // QuerySnapshot<Map<String, dynamic>> msgsCol = await chatDoc.reference
+  //   //     .collection("messages")
+  //   //     .orderBy("time", descending: true)
+  //   //     .get();
+  //   // QueryDocumentSnapshot<Map<String, dynamic>> lastMsg = msgsCol.docs.first;
+  //   // Map<String, dynamic>? lastMsgData = lastMsg.data();
+  //   // print(lastMsgData);
+  //   // //getting the user data
+  //   // List usersIds = chatDoc.data()['users'];
+  //   // String userId = usersIds[0] != thisUserId ? usersIds[0] : usersIds[1];
+  //   // DocumentSnapshot<Map<String, dynamic>> userDoc =
+  //   //     await FirebaseFirestore.instance.collection("users").doc(userId).get();
+  //   // Map<String, dynamic>? userData = userDoc.data();
+  //   // print(userData);
+
+  //   return ChatTile(
+  //       username: userData?['username'],
+  //       profilePhotoLink: userData?['profile_photo'],
+  //       lastMsgText: lastMsgData['text'],
+  //       time: lastMsgData['time']);
+  // }
 
   @override
   void initState() {
@@ -148,49 +171,76 @@ class _MessagingScreenState extends State<MessagingScreen> {
                       ),
                     ),
                   ),
-                  StreamBuilder<List<ChatTile>>(
-                    stream: finalChatTilesStream,
-                    builder: (context, snapshot) {
-                      // print(snapshot.error.toString());
-                      if (snapshot.connectionState == ConnectionState.waiting ||
-                          !snapshot.hasData) {
+                  StreamBuilder<QuerySnapshot>(
+                    stream: getChats(),
+                    builder: (context, chats) {
+                      print(chats.error.toString());
+                      if (chats.connectionState == ConnectionState.waiting ||
+                          !chats.hasData) {
                         return Center(
                           child: CircularProgressIndicator(),
                         );
                       }
-
+                      List<String> chatIds = chats.data!.docs
+                          .toList()
+                          .map(
+                            (e) => e.id,
+                          )
+                          .toList();
                       return Expanded(
                         child: SizedBox(
                           height: 500,
                           child: ListView.builder(
                             itemBuilder: (itemContext, index) {
-                              ChatTile chatTile = snapshot.data![index];
-                              timer = Timer.periodic(
-                                  const Duration(milliseconds: 500), (timer) {
-                                setState(() {
-                                  formattedTime = DateFormat('kk:mm')
-                                      .format(DateTime.now());
-                                });
-                              });
-                              return ListTile(
-                                textColor: Color.fromRGBO(0, 0, 0, 1),
-                                leading: Container(
-                                  width: 50,
-                                  height: 50,
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    image: DecorationImage(
-                                        image: NetworkImage(
-                                            'https://googleflutter.com/sample_image.jpg'),
-                                        fit: BoxFit.fill),
-                                  ),
-                                ),
-                                title: Text(chatTile.username),
-                                subtitle: Text(chatTile.lastMsgText),
-                                trailing: Text(formattedTime),
-                              );
+                              // ChatTile chatTile = snapshot.data![index];
+                              // timer = Timer.periodic(
+                              //     const Duration(milliseconds: 500), (timer) {
+                              //   setState(() {
+                              //     formattedTime = DateFormat('kk:mm')
+                              //         .format(DateTime.now());
+                              //   });
+                              // });
+                              return StreamBuilder<QuerySnapshot>(
+                                  stream: getLastMessages(chatIds[index]),
+                                  builder: (context, messages) {
+                                    if (messages.hasData) {
+                                      return ListTile(
+                                        textColor: Color.fromRGBO(0, 0, 0, 1),
+                                        leading: Container(
+                                          width: 50,
+                                          height: 50,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            image: DecorationImage(
+                                                image: NetworkImage(
+                                                    'https://googleflutter.com/sample_image.jpg'),
+                                                fit: BoxFit.fill),
+                                          ),
+                                        ),
+                                        title: StreamBuilder<
+                                                DocumentSnapshot<
+                                                    Map<String, dynamic>>>(
+                                            stream: getUser(messages
+                                                .data!.docs.first['to']),
+                                            builder: (context, user) {
+                                              if (user.hasData) {
+                                                return Text(user.data!
+                                                    .data()!['username']);
+                                              } else {
+                                                return Text("");
+                                              }
+                                            }),
+                                        subtitle: Text(
+                                            messages.data!.docs.first['text'] ??
+                                                ''),
+                                        trailing: Text(formattedTime),
+                                      );
+                                    } else {
+                                      return Container();
+                                    }
+                                  });
                             },
-                            itemCount: snapshot.data!.length,
+                            itemCount: chatIds.length,
                           ),
                         ),
                       );
