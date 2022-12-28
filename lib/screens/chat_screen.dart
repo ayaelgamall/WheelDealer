bc8678e8960054960a07d0721b1c267117a8dc8c
MoamenShoman <moamenkshoman@gmail.com>
2022-12-28 15:26:19 +0200

    Implemented stream for user changes

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,16 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:bar2_banzeen/models/message.dart';
+import 'package:bar2_banzeen/services/messaging_stream_service.dart';
+import 'package:bar2_banzeen/services/sending_messages_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:grouped_list/grouped_list.dart';
 import 'package:intl/intl.dart';
+
+import '../services/sending_messages_service.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -15,45 +21,92 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  String ChatId = 'PgaDbmblSQzyKmBviEdP';
+  String chatId = 'PgaDbmblSQzyKmBviEdP';
   String thisUserId = 'IKON6R95EWKMNeQbDemX';
+  late String? toUserId;
+  TextEditingController textController = TextEditingController();
 
   Stream<QuerySnapshot> messages() {
     return FirebaseFirestore.instance
         .collection("chats")
-        .doc(ChatId)
+        .doc(chatId)
         .collection("messages")
         .orderBy("time")
         .snapshots();
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          toolbarHeight: 60,
-          leadingWidth: 95,
-          title: Text('Ahmad Khaled'),
-          leading: Row(
-            children: [
-              IconButton(
-                  onPressed: (() {
-                    Navigator.of(context).pop();
-                  }),
-                  icon: Icon(Icons.arrow_back_ios_new_outlined)),
-              Container(
-                width: 45,
-                height: 45,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://googleflutter.com/sample_image.jpg'),
-                      fit: BoxFit.fill),
-                ),
-              ),
-            ],
+          leadingWidth: 35,
+          leading: IconButton(
+              onPressed: (() {
+                Navigator.of(context).pop();
+              }),
+              icon: Icon(Icons.arrow_back_ios_new_outlined)),
+          title: FutureBuilder(
+            future:
+                SendingMessagesService().fetchOtherUserId(chatId, thisUserId),
+            builder: (context, otherUserId) {
+              if (!otherUserId.hasData) {
+                return CircularProgressIndicator();
+              }
+              toUserId = otherUserId.data;
+              return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                  stream: MessagingStreamService().getUser(toUserId.toString()),
+                  builder: (context, user) {
+                    if (!user.hasData) {
+                      return CircularProgressIndicator();
+                    }
+                    return Row(
+                      children: [
+                        Container(
+                          width: 45,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://googleflutter.com/sample_image.jpg'),
+                                fit: BoxFit.fill),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Text(user.data?['username']),
+                      ],
+                    );
+                  });
+            },
           ),
+          // leading: Row(
+          //   children: [
+          //     IconButton(
+          //         onPressed: (() {
+          //           Navigator.of(context).pop();
+          //         }),
+          //         icon: Icon(Icons.arrow_back_ios_new_outlined)),
+          //     Container(
+          //       width: 45,
+          //       height: 45,
+          //       decoration: BoxDecoration(
+          //         shape: BoxShape.circle,
+          //         image: DecorationImage(
+          //             image: NetworkImage(
+          //                 'https://googleflutter.com/sample_image.jpg'),
+          //             fit: BoxFit.fill),
+          //       ),
+          //     ),
+          //     Text('Ahmad Khaled'),
+          //   ],
+          // ),
           actions: [
             Padding(
               padding: const EdgeInsets.only(right: 12.0),
@@ -71,36 +124,110 @@ class _ChatScreenState extends State<ChatScreen> {
               if (!messages.hasData) {
                 return CircularProgressIndicator();
               }
-              return ListView.builder(
-                  itemBuilder: (itemContext, index) {
-                    DateTime dateTime =
-                        (messages.data!.docs[index]['time'] as Timestamp)
-                            .toDate();
-                    return Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      color: thisUserId == messages.data!.docs[index]['from']
-                          ? Colors.blue.shade100
-                          : Colors.grey.shade400,
-                      child: ListTile(
-                        title: Padding(
-                          padding: const EdgeInsets.all(5.0),
-                          child: Text(
-                            thisUserId == messages.data!.docs[index]['from']
-                                ? "You: ${messages.data!.docs[index]['text']}"
-                                : messages.data!.docs[index]['text'],
-                            style: TextStyle(color: Colors.black87),
+              return Column(
+                children: [
+                  Expanded(
+                      child: GroupedListView<QueryDocumentSnapshot<Object?>,
+                          DateTime>(
+                    padding: EdgeInsets.all(8),
+                    reverse: true,
+                    order: GroupedListOrder.DESC,
+                    floatingHeader: true,
+                    elements: messages.data!.docs,
+                    groupBy: (msg) => DateTime(
+                      ((msg['time'] as Timestamp).toDate()).year,
+                      ((msg['time'] as Timestamp).toDate()).month,
+                      ((msg['time'] as Timestamp).toDate()).day,
+                    ),
+                    groupHeaderBuilder: (QueryDocumentSnapshot<Object?> msg) =>
+                        SizedBox(
+                            height: 40,
+                            child: Center(
+                              child: Card(
+                                  color: Colors.amber.shade100,
+                                  child: Padding(
+                                    padding: EdgeInsets.all(8),
+                                    child: Text(
+                                      DateFormat.yMMMd().format(
+                                          (msg['time'] as Timestamp).toDate()),
+                                      style: TextStyle(color: Colors.black54),
+                                    ),
+                                  )),
+                            )),
+                    itemBuilder: (context, QueryDocumentSnapshot<Object?> msg) {
+                      return Align(
+                        alignment: msg['from'] == thisUserId
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
                           ),
+                          color: msg['from'] == thisUserId
+                              ? Color(0xFF02AEB9)
+                              : Color(0xFF4C4C4E),
+                          elevation: 8,
+                          child: Padding(
+                              padding: EdgeInsets.all(14),
+                              child: Text(msg['text'])),
                         ),
-                        trailing: Text(
-                          DateFormat('hh:mm a').format(dateTime),
-                          style: TextStyle(color: Colors.black87),
+                      );
+                    },
+                  )),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    // color: Colors.grey.shade400,
+                    child: TextField(
+                      controller: textController,
+                      style: TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey.shade400,
+                        contentPadding: EdgeInsets.all(12),
+                        hintStyle: TextStyle(color: Colors.black54),
+                        hintText: "Type your message...",
+                        suffixIcon: IconButton(
+                          icon: Icon(Icons.send_rounded),
+                          color: Color.fromARGB(255, 37, 37, 38),
+                          onPressed: () {
+                            Message msg = Message(
+                                from: thisUserId,
+                                to: toUserId.toString(),
+                                text: textController.text,
+                                time: Timestamp.now());
+                            SendingMessagesService().sendMessage(msg, chatId);
+                            textController.clear();
+                          },
                         ),
+                        border: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xFFF5F5F5)),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(90))),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xFFF5F5F5)),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(90))),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Color(0xFFF5F5F5)),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(90))),
                       ),
-                    );
-                  },
-                  itemCount: messages.data!.docs.length);
+                      onSubmitted: (msgText) {
+                        Message msg = Message(
+                            from: thisUserId,
+                            to: toUserId.toString(),
+                            text: msgText,
+                            time: Timestamp.now());
+                        SendingMessagesService().sendMessage(msg, chatId);
+                        textController.clear();
+                      },
+                    ),
+                  )
+                ],
+              );
             }));
   }
 }
