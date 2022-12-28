0352a15b7f22c8315d3b5f7142229870828a006b
basant <basant_allam@hotmail.com>
2022-12-28 21:48:55 +0200

    front end edit profile

--- lib/screens/editProfile.dart
+++ lib/screens/editProfile.dart
@@ -1,6 +1,7 @@
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:bar2_banzeen/services/authentication_service.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({Key? key}) : super(key: key);
@@ -15,6 +16,7 @@ class _EditProfileState extends State<EditProfile> {
   late TextEditingController _emailTextController;
   late TextEditingController _usernameTextController;
   late TextEditingController _nameTextController;
+  late TextEditingController _phoneTextController;
 
   bool _formHasErrors = true;
 
@@ -24,12 +26,44 @@ class _EditProfileState extends State<EditProfile> {
     _emailTextController = TextEditingController();
     _usernameTextController = TextEditingController();
     _nameTextController = TextEditingController();
+    _phoneTextController= TextEditingController();
   }
 
   void updateError(bool err) {
     setState(() {
       _formHasErrors = err;
     });
+  }
+
+  SnackBar createSnackBar(String msg) {
+    return SnackBar(
+      content: Row(
+        children: [
+          const Icon(
+            Icons.error_outline,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 300),
+            child: Text(
+              msg,
+              maxLines: 2,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.red.shade400,
+    );
+  }
+
+  bool validateMobile(String value) {
+    String pattern = r'(^(?:[+2])?[0-9]{12,12}$)';
+    RegExp regExp = new RegExp(pattern);
+   return regExp.hasMatch(value);
   }
 
   @override
@@ -54,14 +88,16 @@ class _EditProfileState extends State<EditProfile> {
                         children: [
                       Row(mainAxisAlignment: MainAxisAlignment.end, children: [
                         const Text("name          "),
-                        Container(padding: const EdgeInsets.all(5),
+                        Container(
+                            padding: const EdgeInsets.all(5),
                             width: 270,
+                            height: 45,
                             child: TextFormField(
                               controller: _nameTextController,
-                              obscureText: true,
                               decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: "name"),
+                                  border:
+                                      UnderlineInputBorder(), // TODO make this line override theme
+                                  hintText: "My current name"),
                             ))
                       ]),
                       const SizedBox(
@@ -69,8 +105,10 @@ class _EditProfileState extends State<EditProfile> {
                       ),
                       Row(mainAxisAlignment: MainAxisAlignment.end, children: [
                         const Text("email         "),
-                        Container(padding: const EdgeInsets.all(5),
+                        Container(
+                            padding: const EdgeInsets.all(5),
                             width: 270,
+                            height: 45,
                             child: TextFormField(
                               controller: _emailTextController,
                               validator: (value) =>
@@ -78,8 +116,9 @@ class _EditProfileState extends State<EditProfile> {
                                       ? null
                                       : "Please enter a valid email address",
                               decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                hintText: "email",
+                                border:
+                                    UnderlineInputBorder(), // TODO make this line override theme
+                                hintText: "Mycurrent@email.com",
                               ),
                             ))
                       ]),
@@ -89,19 +128,72 @@ class _EditProfileState extends State<EditProfile> {
                       Row(mainAxisAlignment: MainAxisAlignment.end, children: [
                         const Text("username "),
                         Container(
-                          padding: const EdgeInsets.all(5),
+                            padding: const EdgeInsets.all(5),
                             width: 270,
+                            height: 45,
                             child: TextFormField(
                               controller: _usernameTextController,
-                              obscureText: true,
                               decoration: const InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  hintText: "username"),
+                                  border:
+                                      UnderlineInputBorder(), // TODO make this line override theme
+                                  hintText: "@username"),
                             ))
                       ]),
                       const SizedBox(
                         height: 20,
-                      )
+                      ),
+                      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                        const Text("phone no. "),
+                        Container(
+                            padding: const EdgeInsets.all(5),
+                            width: 270,
+                            height: 45,
+                            child: TextFormField(
+                              controller: _phoneTextController,
+                              validator: (value) =>
+                                  validateMobile(value!)
+                                      ? null
+                                      : "Please enter a valid Egyptian phone number",
+                              decoration: const InputDecoration(
+                                border:
+                                    UnderlineInputBorder(), // TODO make this line override theme
+                                hintText: "+201234567890",
+                              ),
+                            ))
+                      ]),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 170,
+                        height: 45,
+                        child: ElevatedButton(
+                          onPressed: !_formHasErrors
+                              ? () async {
+                                  // UPDATE DB
+                                  // String? err = await AuthenticationService()
+                                  //     .signInWithEmail(_emailTextController.text,
+                                  //         _passwordTextController.text);
+                                  // if (err != null) {
+                                  //   ScaffoldMessenger.of(context)
+                                  //       .showSnackBar(createSnackBar(err));
+                                  // }
+                                }
+                              : null,
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(7),
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            "Update Profile",
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ),
+                      ),
                     ])))));
   }
 }
