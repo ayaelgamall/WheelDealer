4e803e4a685dd5d7861f5974853e609b1f0f4f27
ayaelgamall <aya.elgamal14@gmail.com>
2023-01-03 21:46:52 +0200

    search bar initial

--- lib/screens/explore_page.dart
+++ lib/screens/explore_page.dart
@@ -1,7 +1,10 @@
+import 'package:bar2_banzeen/widgets/explore_Page_Content.dart';
+import 'package:bar2_banzeen/widgets/search_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/scrollable_cars.dart';
+import '../widgets/search_bar.dart';
 
 class ExplorePage extends StatefulWidget {
   const ExplorePage({Key? key}) : super(key: key);
@@ -13,55 +16,23 @@ class ExplorePage extends StatefulWidget {
 
 class _ExplorePageState extends State<ExplorePage> {
 
-
   @override
   Widget build(BuildContext context) {
     // final cars = FirebaseFirestore.instance.collection('cars');
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
-    String selected = 'Top searches';
-    final cars = FirebaseFirestore.instance.collection('cars');
+
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        margin: EdgeInsets.only(top: 20, left: 20, right: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
 
-              children: [
-                // Text(selected,style: Theme.of(context).textTheme.labelMedium,),
-
-                Directionality(
-                  textDirection: TextDirection.rtl,
-                  child: ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.arrow_drop_down),
-                  label: Text('sort'),),
-                ),
-                SizedBox(width: 10,),
-                ElevatedButton(onPressed: (){}, child: Text('filter'))
-              ],
-            ),
-            Expanded(
-              child: SizedBox(
-                width: 0.85 * width,
-
-                child: ScrollableCars(
-                  width: 0.85 * width,
-                  height: 0.3 * height,
-                  carsToShow: cars
-                      .orderBy("bids_count", descending: true),//todo change
-                  align: Axis.vertical,
-                  rightMargin: 0,
-                ),
-              ),
-            ),
-            // CarCard(width: 0.8 * width, height: 0.3 * height, rightMargin: )
-
-          ],
-        ),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: CustomSearchDelegate());
+            },
+          )
+        ],
       ),
+      body: ExploreContent(),
     );
   }
 }
