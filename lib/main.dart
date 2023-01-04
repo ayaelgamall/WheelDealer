9a227a0b0ac7a3cc8d45b2d524c4c996b919ff62
basant <basant_allam@hotmail.com>
2023-01-04 22:37:05 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -115,8 +115,8 @@ class _MyAppState extends State<MyApp> {
           GoRoute(
             path: '/sellCar',
             builder: (BuildContext context, GoRouterState state) {
-              // return SellCarScreen(); //TODO REMOVE DUMMY
-              return  SellCarScreen(carId: "3EQL9bSGFwnUtlNaq24h",); //TODO REMOVE DUMMY
+              return SellCarScreen(); //TODO REMOVE DUMMY
+              // return  SellCarScreen(carId: "3EQL9bSGFwnUtlNaq24h",); //TODO REMOVE DUMMY
             },
             // routes: <RouteBase>[
             //   // The details screen to display stacked on the inner Navigator.
